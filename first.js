8e2f892ee8802cbd17c142307d352fb4f23f0577
Khamz <khamzzaty@mail.ru>
2023-05-22 13:08:47 +0300

    back

---
+++ first.js
@@ -0,0 +1,7 @@
+export let numOne = 5;
+export let numTwo = 1
+
+export default function khamz() {
+    return 2 + 2
+}
+
---
+++ second.js
@@ -0,0 +1,3 @@
+import khamz,{numOne, numTwo} from "./first.js"
+console.log(numOne+numTwo)
+console.log(khamz())
